a2e358db35da0b0b29067c54d33d092ddced9add
PP09PetyaDecryptor <166217226+PP09PetyaDecryptor@users.noreply.github.com>
2024-05-07 21:31:27 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -60,8 +60,8 @@ async function play(client, interaction) {
         } else {
             const errorEmbed = new EmbedBuilder()
                 .setColor('#ff0000')
-                .setTitle('Error')
-                .setDescription('There are no results found.');
+                .setTitle('Blad!')
+                .setDescription('Nie znaleziono muzyki.');
 
         
             await interaction.editReply({ embeds: [errorEmbed] });
@@ -80,31 +80,31 @@ const embeds = [
     new EmbedBuilder()
         .setColor('#4d9fd6')
         .setAuthor({
-            name: 'Request Update!',
+            name: 'Dodano piosenkę!',
             iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236794583732457473/7828-verify-ak.gif?ex=66394e37&is=6637fcb7&hm=923d3f3b300606a2ae4ceb7bae980fd533a4c5ee2cf73111569a892a595f1f69&', 
-            url: 'https://discord.gg/xQF9f9yUEM'
+            url: 'a'
         })
-        .setDescription('➡️ **Your request has been successfully processed.**\n➡️** Please use the buttons to control the queue**'),
+        .setDescription('➡️ **Dodano piosenke do kolejki.**\n➡️** Uzyj przyciskow ponizej aby kontrolowac kolejke muzyki.**'),
 
  
     new EmbedBuilder()
     .setColor('#ffea00')
     .setAuthor({
-        name: 'Request Update!',
+        name: 'Dodano piosenke!',
         iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802032938127470/4104-verify-yellow.gif?ex=66395527&is=663803a7&hm=71a7fba7f91897e52d9645b45d85d3da0ff97af2b63d10960004e68ff40d9c3b&', 
-        url: 'https://discord.gg/xQF9f9yUEM'
+        url: 'a'
     })
-    .setDescription('➡️ **Your request has been successfully processed.**\n➡️** Please use the buttons to control the queue**'),
+    .setDescription('➡️ **Dodano piosenke do kolejki.**\n➡️** Uzyj przyciskow ponizej aby kontrolowac kolejke muzyki.**'),
 
   
     new EmbedBuilder()
     .setColor('#FF0000')
     .setAuthor({
-        name: 'Request Update!',
+        name: 'Dodano piosenke!',
         iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=6639552b&is=663803ab&hm=8dbc851fe56441b916a0044152dd517ed26434a11ee506518382f380c527c3bd&', 
-        url: 'https://discord.gg/xQF9f9yUEM'
+        url: 'a'
     })
-    .setDescription('➡️ **Your request has been successfully processed.**\n➡️** Please use the buttons to control the queue**')
+    .setDescription('➡️ **Dodano piosenke do kolejki.**\n➡️** Uzyj przyciskow ponizej aby kontrolowac kolejke muzyki.**')
 ];
 
 
@@ -114,11 +114,11 @@ const randomIndex = Math.floor(Math.random() * embeds.length);
 await interaction.followUp({ embeds: [embeds[randomIndex]] });
 
     } catch (error) {
-        console.error('Error processing play command:', error);
+        console.error('Blad wywolania komendy /play:', error);
         const errorEmbed = new EmbedBuilder()
             .setColor('#ff0000')
             .setTitle('Error')
-            .setDescription('An error occurred while processing your request.');
+            .setDescription('Wystapil problem z wywolaniem komendy /play.');
 
      
         await interaction.editReply({ embeds: [errorEmbed] });
@@ -127,11 +127,11 @@ await interaction.followUp({ embeds: [embeds[randomIndex]] });
 
 module.exports = {
     name: "play",
-    description: "Add options too",
+    description: "utwor",
     permissions: "0x0000000000000800",
     options: [{
         name: 'name',
-        description: 'enter song name / link or playlist',
+        description: 'Napisz nazwe piosenki, link do piosenki albo playliste.',
         type: ApplicationCommandOptionType.String,
         required: true
     }],
