663999102655bf7a09428a33184aa6398063851d
PP09PetyaDecryptor <166217226+PP09PetyaDecryptor@users.noreply.github.com>
2024-05-07 21:33:25 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -22,7 +22,7 @@ const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
   name: "ping",
-  description: "check the bot latency",
+  description: "Zobacz ping bota.",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
@@ -31,11 +31,11 @@ module.exports = {
     try {
 
       const start = Date.now();
-      interaction.reply("Pinging....").then(msg => {
+      interaction.reply("Pingowanie....").then(msg => {
         const end = Date.now();
         const embed = new EmbedBuilder()
           .setColor(`#6190ff`)
-          .setTitle(`Bot Latency`)
+          .setTitle(`Pig bota`)
           .setDescription(`**Pong** : ${end - start}ms`)
         return interaction.editReply({ embeds: [embed] }).catch(e => { });
       }).catch(err => { })
