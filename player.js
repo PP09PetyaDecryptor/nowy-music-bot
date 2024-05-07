4adee0f841f1d04d4b0a2ef356383be83dbee885
PP09PetyaDecryptor <166217226+PP09PetyaDecryptor@users.noreply.github.com>
2024-05-07 21:43:18 +0200

    Update player.js

--- player.js
+++ player.js
@@ -68,7 +68,7 @@ function initializePlayer(client) {
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=663939fa&is=6637e87a&hm=e02431de164b901e07b55d8f8898ca5b1b2832ad11985cecc3aa229a7598d610&`)
         .setThumbnail(track.info.thumbnail)
         .setTimestamp()
-        .setFooter({ text: 'Kliknij przyciski ponizej aby kontrolować muzyke!')
+        .setFooter({ text: 'Kliknij przyciski ponizej aby kontrolować muzyke!';
 
 
 
